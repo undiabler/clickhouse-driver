dc509530b6a3e5180c5692e55b35c4408f47ce86
undiabler <undiabler@gmail.com>
2018-05-05 20:18:48 +0200

    test docs

--- conn.go
+++ conn.go
@@ -10,12 +10,14 @@ const (
 	successTestResponse = "1"
 )
 
+// Connection struct passing query via transport
 type Conn struct {
 	Host      string
 	transport Transport
 	params    url.Values
 }
 
+// NewConn creates default connection to db
 func NewConn(host string, t Transport) *Conn {
 	if strings.Index(host, "http://") < 0 && strings.Index(host, "https://") < 0 {
 		host = "http://" + host
@@ -29,6 +31,7 @@ func NewConn(host string, t Transport) *Conn {
 	}
 }
 
+// NewAuthConn creates connection with user/pass auth params
 func NewAuthConn(host string, t Transport, user, pass string) *Conn {
 	conn := NewConn(host, t)
 	conn.AddParam("user", user)
@@ -36,6 +39,8 @@ func NewAuthConn(host string, t Transport, user, pass string) *Conn {
 	return conn
 }
 
+// Test connection
+// TODO: calculate query time for cluster ranking
 func (c *Conn) Ping() (err error) {
 	var res string
 	res, err = c.Exec(Query{Stmt: "SELECT+1"}, true)
@@ -48,6 +53,7 @@ func (c *Conn) Ping() (err error) {
 	return err
 }
 
+// Exec pass query to self transport
 func (c *Conn) Exec(q Query, readOnly bool) (res string, err error) {
 	return c.transport.Exec(c.GetHost(), c.GetParams().Encode(), q, readOnly)
 }
@@ -56,14 +62,18 @@ func (c *Conn) SetParams(params url.Values) {
 	c.params = params
 }
 
+// AddParam add extra param to query such as max execution time etc.
+// For more params read clickhouse.yandex docs
 func (c *Conn) AddParam(key string, value string) {
 	c.params.Set(key, value)
 }
 
+// GetParams return all extra params for query
 func (c *Conn) GetParams() url.Values {
 	return c.params
 }
 
+// GetHost return connection hostname, usefull for clusters
 func (c *Conn) GetHost() string {
 	return c.Host
 }
