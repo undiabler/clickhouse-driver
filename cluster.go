394ca672906e3abc66829ca2c18272086e25f880
undiabler <undiabler@gmail.com>
2018-05-06 18:34:05 +0200

    docs

--- cluster.go
+++ cluster.go
@@ -7,6 +7,7 @@ import (
 
 type PingErrorFunc func(*Conn)
 
+// Cluster is useful if you have several DBs with distributed or partitional logic. In this case you can send requests to random server to load balance and improve stability.
 type Cluster struct {
 	conn   []*Conn
 	active []*Conn
@@ -14,22 +15,30 @@ type Cluster struct {
 	mx     sync.Mutex
 }
 
+// NewCluster create cluster from connections
 func NewCluster(conn ...*Conn) *Cluster {
 	return &Cluster{
 		conn: conn,
 	}
 }
 
+// IsDown check if there at least one working connection
 func (c *Cluster) IsDown() bool {
 	c.mx.Lock()
 	defer c.mx.Unlock()
 	return len(c.active) < 1
 }
 
+// OnCheckError callback func on each fail ping
+//
+// TODO: same func for all cluster down
 func (c *Cluster) OnCheckError(f PingErrorFunc) {
 	c.fail = f
 }
 
+// ActiveConn return random active connection
+//
+// TODO: same func for best conn speed
 func (c *Cluster) ActiveConn() *Conn {
 	c.mx.Lock()
 	defer c.mx.Unlock()
@@ -40,6 +49,7 @@ func (c *Cluster) ActiveConn() *Conn {
 	return c.active[rand.Intn(l)]
 }
 
+// Check call Ping for all connections and save active
 func (c *Cluster) Check() {
 	var (
 		err error
