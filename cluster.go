30be5396308d77225615fa7e3b40741e9a5f45c3
UndiAbler <undiabler@gmail.com>
2018-05-16 17:29:45 +0200

    preparing for ping stats check

--- cluster.go
+++ cluster.go
@@ -3,22 +3,53 @@ package clickhouse
 import (
 	"math/rand"
 	"sync"
+	"time"
 )
+
+type pingStats struct {
+	mx     sync.Mutex
+	count  int
+	errors int
+	last   int64
+	avg    float64
+}
+
+func (p *pingStats) NewCheck(last int64, err bool) {
+	p.mx.Lock()
+	defer p.mx.Unlock()
+	if err {
+		p.errors++
+		return
+	}
+	p.last = last
+	if p.count == 0 {
+		p.avg = float64(last)
+	} else {
+		p.avg = (p.avg*float64(p.count) + float64(last)) / (float64(p.count) + 1)
+	}
+	p.count += 1
+}
 
 type PingErrorFunc func(*Conn)
 
 // Cluster is useful if you have several DBs with distributed or partitional logic. In this case you can send requests to random server to load balance and improve stability.
 type Cluster struct {
-	conn   []*Conn
-	active []*Conn
-	fail   PingErrorFunc
+	conn map[*Conn]*pingStats
+
 	mx     sync.Mutex
+	active []*Conn
+
+	fail PingErrorFunc
 }
 
 // NewCluster create cluster from connections
 func NewCluster(conn ...*Conn) *Cluster {
+	conns := make(map[*Conn]*pingStats)
+	for i := range conn {
+		conns[conn[i]] = &pingStats{}
+	}
 	return &Cluster{
-		conn: conn,
+		conn: conns,
 	}
 }
 
@@ -56,8 +87,14 @@ func (c *Cluster) Check() {
 		res []*Conn
 	)
 
-	for _, conn := range c.conn {
+	for conn, val := range c.conn {
+
+		// measure ping time
+		start := time.Now()
 		err = conn.Ping()
+		elapsed := time.Since(start)
+		val.NewCheck(elapsed.Nanoseconds(), err != nil)
+
 		if err == nil {
 			res = append(res, conn)
 		} else {
